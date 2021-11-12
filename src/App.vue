af8c7e40c0e18563a846fe69895457baeb4ca8b6
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-12 19:06:30 +0200

    TodoList page was created

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-  <h1>Todo application</h1>
+    <h1>Todo application</h1>
+    <TodoList />
   </div>
 </template>
 
+
 <script>
+import TodoList from "./components/TodoList.vue";
+
 export default {
   name: "App",
+  components: {
+    TodoList,
+  },
 };
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template lang="en">
+<div>
+    <ul>
+        <li>todo 1</li>
+    </ul>
+</div>
+</template>
+<script>
+export default {
+  name: "TodoList",
+};
+</script>
+
+<style>
+    ul {
+        padding: 0;
+    }
+
+    li {
+        color: blue;
+    }
+</style>
